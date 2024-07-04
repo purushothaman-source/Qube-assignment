6613375d4b838699b40e98f95c9b7236c8987cc4
Purushoth <purushothaman.s@getedge.ai>
2024-07-04 16:20:07 +0530

    Fix cors issue

--- mock-api-server/src/index.js
+++ mock-api-server/src/index.js
@@ -1,8 +1,16 @@
 const express = require("express");
+const cors = require("cors");
 
 const applianceRoutes = require("./routes/applianceRoutes");
 
 const app = express();
+
+const corsOptions = {
+  origin: process.env.FRONTEND_URL, // Allow only this origin
+  methods: "GET,POST,DELETE,PUT", // Allow only GET and POST requests
+};
+
+app.use(cors(corsOptions));
 
 const port = process.env.PORT || 3001;
 
